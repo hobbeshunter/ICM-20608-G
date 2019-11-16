9d62e37751e2f58fdf7522fc954e8919b064b918
Martin Wudenka <Martin.Wudenka@gmx.de>
2019-11-16 17:16:03 +0100

    style: remove unneeded line

--- src/ICM-20608-G.h
+++ src/ICM-20608-G.h
@@ -4,7 +4,6 @@
 #include <SPI.h>
 
 #define CS D10
-#define GYRO_LSB_TO_DEGS_1000 (1000.f / 32768.f)
 
 enum GyroRange
 {
@@ -39,6 +38,13 @@ public:
 
     void readAccel(float &x, float &y, float &z) const;
 
+    int16_t offset_gx;
+    int16_t offset_gy;
+    int16_t offset_gz;
+    int16_t offset_ax;
+    int16_t offset_ay;
+    int16_t offset_az;
+
 private:
     SPISettings settings;
 
@@ -48,13 +54,6 @@ private:
 
     float gyro_lsb_to_degs;
     float accel_lsb_to_g;
-
-    int16_t offset_gx;
-    int16_t offset_gy;
-    int16_t offset_gz;
-    int16_t offset_ax;
-    int16_t offset_ay;
-    int16_t offset_az;
 
     void readGyroRaw(int16_t &x, int16_t &y, int16_t &z) const;
     void readAccelRaw(int16_t &x, int16_t &y, int16_t &z) const;
